a242827a914016e5c1a65c58a89d427ce785a21c
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-06-10 01:29:58 +0300

    a

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Header } from "components/Header/Header";
 import { Outlet } from "react-router-dom";
+
 export const Layout = () => {
-    return <>
+    return <div className="container">
     <Header/>
     <Outlet/>
-    </>
+    </div>
 }
--- src/components/trending/trending.jsx
+++ src/components/trending/trending.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 export const Trending = () => {
     return <div>
-        This is Trending
+        Trending today
     </div>
 }
